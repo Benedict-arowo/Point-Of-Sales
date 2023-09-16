8c5ce6c7fda805812b8641b4ddca0bf508ecbdda
Benedict <benedict.arowo@gmail.com>
2023-09-16 12:11:26 +0300

    Cleaning up code, and fixing small bugs

--- backend/dist/Controller/itemsController.js
+++ backend/dist/Controller/itemsController.js
@@ -15,20 +15,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteItem = exports.patchItem = exports.getItem = exports.addItem = exports.getItems = void 0;
 const prismaClient_1 = __importDefault(require("../DB/prismaClient"));
 const http_status_codes_1 = require("http-status-codes");
+const addItem_1 = __importDefault(require("../Services/Item/addItem"));
+const patchItem_1 = __importDefault(require("../Services/Item/patchItem"));
 const isNumber = (value) => {
     // Returns true if the value is a number, and false otherwise.
     return typeof value === "number";
 };
 const getItems = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { name, price } = req.query;
+    const { name } = req.query;
     const items = yield prismaClient_1.default.items.findMany({
         where: {
             name: {
-                contains: name,
-            },
-            price: {
-                gte: price ? Number(price) : undefined,
-                // TODO: Change the price to number and assign it to a variable and add error checking
+                startsWith: name,
+                mode: "insensitive",
             },
         },
     });
@@ -38,58 +37,8 @@ const getItems = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 });
 exports.getItems = getItems;
 const addItem = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(req.body);
-    const { name, price, description, amountInStock, priceBought, category } = req.body;
-    const CategoryList = ["FOOD", "DRINK", "GAMES"];
-    if (!name || !price || !priceBought || !category) {
-        return new Error("Name, Price, Description, PriceBought and Category are all required fields.");
-    }
-    if (category && !CategoryList.includes(category.toUpperCase())) {
-        return res
-            .json({ msg: "Invalid Category." })
-            .status(http_status_codes_1.StatusCodes.BAD_REQUEST);
-        // return new Error("Invalid Category.");
-    }
-    // TODO: A better way of checking if typeof an variable is a number.
-    if (isNumber(price)) {
-        console.log(1);
-        return res
-            .json({ msg: "Price must be a number." })
-            .status(http_status_codes_1.StatusCodes.BAD_REQUEST);
-        // return new Error("Price must be a number.");
-    }
-    if (isNumber(priceBought)) {
-        console.log(2);
-        return res
-            .json({ msg: "Price Bought must be a number." })
-            .status(http_status_codes_1.StatusCodes.BAD_REQUEST);
-        // return new Error("Price must be a number.");
-    }
-    if (isNumber(amountInStock)) {
-        console.log(3);
-        return res
-            .json({ msg: "Amount in Stock must be a number." })
-            .status(http_status_codes_1.StatusCodes.BAD_REQUEST);
-        // return new Error("amountInStock must be a number.");
-    }
-    console.log("Done checking");
-    // TODO: Better error checking...
-    const item = yield prismaClient_1.default.items.create({
-        data: {
-            name,
-            price: Number(price),
-            description,
-            amountInStock: Number(amountInStock),
-            priceBought: Number(priceBought),
-            category: "FOOD",
-        },
-        select: {
-            id: true,
-            name: true,
-            created: true,
-        },
-    });
-    return res.json({ msg: "success", data: item }).status(201);
+    const data = yield (0, addItem_1.default)(req, res);
+    return res.json({ msg: "success", data }).status(201);
 });
 exports.addItem = addItem;
 const getItem = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -108,44 +57,14 @@ const getItem = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 exports.getItem = getItem;
 const patchItem = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     // TODO: Permission checking
-    const { id } = req.params;
-    const { name, amountInStock, price, priceBought, category, description } = req.body;
-    if (!name) {
-        throw new Error("Name must be provided.");
-    }
-    if (!price) {
-        throw new Error("Price must be provided.");
-    }
-    else if (!isNumber(price))
-        throw new Error("Price must be a number.");
-    if (!amountInStock) {
-        throw new Error("Amount in Stock must be provided.");
-    }
-    else if (!isNumber(amountInStock))
-        throw new Error("Amount in stock must be a number.");
-    if (!priceBought) {
-        throw new Error("Price bought must be provided.");
-    }
-    else if (!isNumber(priceBought))
-        throw new Error("Price bought must be a number.");
-    const item = yield prismaClient_1.default.items.update({
-        where: { id },
-        data: {
-            name,
-            amountInStock,
-            price,
-            priceBought,
-            category,
-            description,
-        },
-    });
-    return res.json({ msg: "success", data: item }).status(http_status_codes_1.StatusCodes.OK);
+    const data = yield (0, patchItem_1.default)(req);
+    return res.json({ msg: "success", data }).status(http_status_codes_1.StatusCodes.OK);
 });
 exports.patchItem = patchItem;
 const deleteItem = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     // TODO: permission checking
     const { id } = req.params;
-    yield prismaClient_1.default.items.delete({
+    yield prismaClient_1.default.items.deleteMany({
         where: { id },
     });
     return res.json({ message: "success" }).status(http_status_codes_1.StatusCodes.OK);
--- backend/src/Controller/itemsController.ts
+++ backend/src/Controller/itemsController.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import prisma from "../DB/prismaClient";
 import { StatusCodes } from "http-status-codes";
 import { CategoryList } from "@prisma/client";
-
-interface Req extends Request {
+import addItemService from "../Services/Item/addItem";
+import patchItemService from "../Services/Item/patchItem";
+export interface Req extends Request {
 	query: {
 		name?: string;
 		price?: string;
@@ -13,7 +14,8 @@ interface Req extends Request {
 		name?: string;
 		price?: number;
 		description?: string;
-		amountInStock?: number;
+		unitsInStock?: number;
+		pricePerUnit?: number;
 		priceBought?: number;
 		category: CategoryList;
 	};
@@ -25,16 +27,13 @@ const isNumber = (value: any) => {
 };
 
 export const getItems = async (req: Req, res: Response) => {
-	const { name, price } = req.query;
+	const { name } = req.query;
 
 	const items = await prisma.items.findMany({
 		where: {
 			name: {
-				contains: name,
-			},
-			price: {
-				gte: price ? Number(price) : undefined,
-				// TODO: Change the price to number and assign it to a variable and add error checking
+				startsWith: name,
+				mode: "insensitive",
 			},
 		},
 	});
@@ -45,67 +44,8 @@ export const getItems = async (req: Req, res: Response) => {
 };
 
 export const addItem = async (req: Req, res: Response) => {
-	console.log(req.body);
-	const { name, price, description, amountInStock, priceBought, category } =
-		req.body;
-	const CategoryList = ["FOOD", "DRINK", "GAMES"];
-
-	if (!name || !price || !priceBought || !category) {
-		return new Error(
-			"Name, Price, Description, PriceBought and Category are all required fields."
-		);
-	}
-
-	if (category && !CategoryList.includes(category.toUpperCase())) {
-		return res
-			.json({ msg: "Invalid Category." })
-			.status(StatusCodes.BAD_REQUEST);
-		// return new Error("Invalid Category.");
-	}
-
-	// TODO: A better way of checking if typeof an variable is a number.
-	if (isNumber(price)) {
-		console.log(1);
-		return res
-			.json({ msg: "Price must be a number." })
-			.status(StatusCodes.BAD_REQUEST);
-		// return new Error("Price must be a number.");
-	}
-
-	if (isNumber(priceBought)) {
-		console.log(2);
-		return res
-			.json({ msg: "Price Bought must be a number." })
-			.status(StatusCodes.BAD_REQUEST);
-		// return new Error("Price must be a number.");
-	}
-
-	if (isNumber(amountInStock)) {
-		console.log(3);
-		return res
-			.json({ msg: "Amount in Stock must be a number." })
-			.status(StatusCodes.BAD_REQUEST);
-		// return new Error("amountInStock must be a number.");
-	}
-	console.log("Done checking");
-	// TODO: Better error checking...
-	const item = await prisma.items.create({
-		data: {
-			name,
-			price: Number(price),
-			description,
-			amountInStock: Number(amountInStock),
-			priceBought: Number(priceBought),
-			category: "FOOD",
-		},
-		select: {
-			id: true,
-			name: true,
-			created: true,
-		},
-	});
-
-	return res.json({ msg: "success", data: item }).status(201);
+	const data = await addItemService(req, res);
+	return res.json({ msg: "success", data }).status(201);
 };
 
 export const getItem = async (req: Req, res: Response) => {
@@ -125,46 +65,14 @@ export const getItem = async (req: Req, res: Response) => {
 
 export const patchItem = async (req: Req, res: Response) => {
 	// TODO: Permission checking
-	const { id } = req.params;
-	const { name, amountInStock, price, priceBought, category, description } =
-		req.body;
-
-	if (!name) {
-		throw new Error("Name must be provided.");
-	}
-
-	if (!price) {
-		throw new Error("Price must be provided.");
-	} else if (!isNumber(price)) throw new Error("Price must be a number.");
-
-	if (!amountInStock) {
-		throw new Error("Amount in Stock must be provided.");
-	} else if (!isNumber(amountInStock))
-		throw new Error("Amount in stock must be a number.");
-
-	if (!priceBought) {
-		throw new Error("Price bought must be provided.");
-	} else if (!isNumber(priceBought))
-		throw new Error("Price bought must be a number.");
-
-	const item = await prisma.items.update({
-		where: { id },
-		data: {
-			name,
-			amountInStock,
-			price,
-			priceBought,
-			category,
-			description,
-		},
-	});
-	return res.json({ msg: "success", data: item }).status(StatusCodes.OK);
+	const data = await patchItemService(req);
+	return res.json({ msg: "success", data }).status(StatusCodes.OK);
 };
 
 export const deleteItem = async (req: Request, res: Response) => {
 	// TODO: permission checking
 	const { id } = req.params;
-	await prisma.items.delete({
+	await prisma.items.deleteMany({
 		where: { id },
 	});
 	return res.json({ message: "success" }).status(StatusCodes.OK);
