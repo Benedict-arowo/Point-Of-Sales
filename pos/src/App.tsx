e10a9c135c7a5083026b2e4d353272b97f80f296
Benedict <benedict.arowo@gmail.com>
2023-09-17 10:45:58 +0300

    + ReactRouter for Routing

--- pos/src/App.tsx
+++ pos/src/App.tsx
@@ -1,15 +1,36 @@
 // import React from "react";
 import Menu from "./Components/Menu.tsx";
 import Home from "./Pages/Home.tsx";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Inventory from "./Pages/Inventory.tsx";
+import Reports from "./Pages/Reports.tsx";
+import Settings from "./Pages/Settings.tsx";
+import Bills from "./Pages/Bills.tsx";
+import Login from "./Pages/Login.tsx";
 
 const App = () => {
+	return (
+		<Routes>
+			<Route element={<MenuElement />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/inventory" element={<Inventory />} />
+				<Route path="/bills" element={<Bills />} />
+				<Route path="/settings" element={<Settings />} />
+				<Route path="/reports" element={<Reports />} />
+			</Route>
+			<Route path="/login" element={<Login />} />
+		</Routes>
+	);
+};
+
+//* For pages like login where the menu is not needed.
+const MenuElement = () => {
 	return (
 		<div className="flex h-screen text-black">
 			<div className="w-28">
 				<Menu />
 			</div>
-			{/* Routing here */}
-			<Home />
+			<Outlet />
 		</div>
 	);
 };
