8543186f1725910f19bf880ddb2a65c7088218ff
Benedict <benedict.arowo@gmail.com>
2023-09-24 14:59:58 +0300

    Inventory Page UI

--- pos/src/Pages/Inventory.tsx
+++ pos/src/Pages/Inventory.tsx
@@ -53,54 +53,97 @@ const Inventory = () => {
 		}
 
 		return Items.map((item) => {
+			console.log(item);
 			const {
 				id,
 				name,
-				description,
 				pricePerUnit,
-				priceBought,
 				category,
+				description,
 				unitsInStock,
-				reorderLevel,
 				isOutOfStock,
 				initialQuantity,
-				estimatedProfit,
 				created,
 				updated,
 			} = item;
 			return (
 				<div
 					key={id}
-					className="px-4 py-1 border rounded-sm shadow-md h-fit w-[280px]">
+					className="px-4 py-1 border rounded-sm shadow-md h-fit w-full pb-2 max-w-4xl">
 					<header className="flex flex-row justify-between items-center w-full my-2">
-						<h6 className="font-semibold text-2xl text-center my-2 self-center">
+						<h6 className="font-semibold cursor-pointer text-2xl text-center my-2 self-center">
 							{name}
 						</h6>
 						{isOutOfStock ? (
-							<p className="bg-green-600 text-white font-semibold w-fit h-fit px-2 py-1 text-xs rounded-md">
+							<p className="bg-green-600 cursor-default text-white font-semibold w-fit h-fit px-2 py-1 text-xs rounded-md">
 								IN STOCK
 							</p>
 						) : (
-							<p className="bg-rose-500 text-white font-semibold w-fit h-fit px-2 py-1 text-xs rounded-md">
+							<p className="bg-rose-500 cursor-default text-white font-semibold w-fit h-fit px-2 py-1 text-xs rounded-md">
 								OUT OF STOCK
 							</p>
 						)}
 					</header>
-					<p>{description}</p>
-					<p>{category}</p>
-					<div>
-						<p>Price: {pricePerUnit}</p>
-						<p>Price Bought: {priceBought}</p>
-					</div>
-					<div>
-						<p>Units In Stock: {unitsInStock}</p>
-						<p>Reorder Level: {reorderLevel}</p>
-						<p>Initial Quantity: {initialQuantity}</p>
-						<p>Estimated Profit: {estimatedProfit}</p>
-						<p>Amount Sold: {initialQuantity - unitsInStock}</p>
-						<p>Created: {format(created)}</p>
-						<p>Updated: {format(updated)}</p>
-					</div>
+					<div className="border w-full mb-2 mt-1 border-[#E5E5E5]"></div>
+					{description && (
+						<div className="w-full text-sm mb-2">{description}</div>
+					)}
+					<main className="w-full flex flex-row justify-between cursor-default">
+						<div>
+							<div className="flex flex-row gap-2 items-center font-light">
+								<p className="font-medium text-lg">Price: </p>
+								<span>&#8358;{pricePerUnit}</span>
+							</div>
+
+							<div className="flex flex-row gap-2 items-center font-light">
+								<p className="font-medium text-lg">
+									Amount Sold:
+								</p>
+								<span> {initialQuantity - unitsInStock}</span>
+							</div>
+
+							<div className="flex flex-row gap-2 items-center font-light">
+								<p className="font-medium text-lg">
+									Amount Sold Today:
+								</p>
+								<span>{initialQuantity - unitsInStock}</span>
+							</div>
+						</div>
+
+						<div>
+							<div className="flex flex-row gap-2 items-center">
+								<p className="font-medium text-lg">
+									Last Restock:
+								</p>
+								<span className="font-light">
+									{pricePerUnit}
+								</span>
+							</div>
+							<div className="flex flex-row gap-2 items-center">
+								<p className="font-medium text-lg">Category:</p>
+								<span className="font-light">{category}</span>
+							</div>
+							{/* <div className="flex flex-row gap-2 items-center">
+								<p className="font-medium text-lg">Created:</p>
+								<span className="font-light">
+									{format(created)}
+								</span>
+							</div> */}
+						</div>
+					</main>
+					<div className="border w-full mt-1 border-[#E5E5E5]"></div>
+					<footer className="mt-2 text-xs flex flex-row justify-between w-full">
+						<div className="flex flex-row gap-2">
+							<p className="font-medium">Created:</p>
+							<span className="">{format(created)}</span>
+						</div>
+						{created !== updated && (
+							<div className="flex flex-row gap-2">
+								<p className="font-medium">Last Updated:</p>
+								<span className="">{format(updated)}</span>
+							</div>
+						)}
+					</footer>{" "}
 				</div>
 			);
 		});
@@ -110,8 +153,8 @@ const Inventory = () => {
 	) : err === null ? (
 		<div>Error</div>
 	) : (
-		<div className="bg-[#ffffffbb] px-4 w-full h-screen overflow-y-scroll">
-			<div className="px-2 py-2 mt-4 mx-auto max-w-xl flex flex-row justify-between border-2 border-[#8A8A8A] rounded-sm">
+		<div className="bg-[#ffffffbb] w-full h-screen px-4 overflow-y-scroll">
+			<div className="px-2 py-2 mt-4 mx-auto max-w-lg flex flex-row justify-between border-2 border-[#8A8A8A] rounded-sm">
 				<input
 					className="text-gray-500 w-full outline-none"
 					type="text"
@@ -135,8 +178,10 @@ const Inventory = () => {
 				</svg>
 			</div>
 
+			<div>{/* IN STOCK && OUT OF STOCK SORTING */}</div>
+
 			{/* <section className="flex flex-gap-3 justify-center flex-wrap mt-6"> */}
-			<section className="px-16 mt-8 flex flex-row flex-wrap gap-12 h-screen justify-center mb-8">
+			<section className="px-16 mt-8 flex flex-col gap-12 items-center mb-8">
 				<DisplayInventoryItems />
 			</section>
 		</div>
